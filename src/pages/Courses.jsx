e2fd1dc02fd6580e9cc9a0d43ad0922e3c865ac2
MdHussain22 <mdhussain403913@gmail.com>
2026-01-11 09:54:36 +0530

    main

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,25 +1,41 @@
 import Navbar from "../components/Navbar";
 import CourseCard from "../components/CourseCard";
 
+const courses = [
+  {
+    id: 1,
+    title: "Java Basics",
+    description: "Start your programming journey",
+  },
+  {
+    id: 2,
+    title: "Web Development",
+    description: "HTML, CSS, JavaScript & more",
+  },
+  {
+    id: 3,
+    title: "SQL for Beginners",
+    description: "Learn databases from scratch",
+  },
+];
+
 export default function Courses() {
   return (
     <div>
       <Navbar />
-      <div style={{ padding: 40 }}>
-        <h1>Courses</h1>
 
-        <CourseCard
-          title="Java Basics"
-          description="Start your programming journey"
-        />
-        <CourseCard
-          title="Web Development"
-          description="HTML, CSS, JavaScript & more"
-        />
-        <CourseCard
-          title="SQL for Beginners"
-          description="Learn databases from scratch"
-        />
+      <div style={{ padding: 40 }}>
+        <h1>All Courses</h1>
+
+        <div style={{ display: "flex", gap: 20, marginTop: 20 }}>
+          {courses.map((course) => (
+            <CourseCard
+              key={course.id}
+              title={course.title}
+              description={course.description}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
